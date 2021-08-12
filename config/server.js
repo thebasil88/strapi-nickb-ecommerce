a57537f1506d65beefa6cf63605f68074d0f1bf4
Nick Basil <thebasil88@gmail.com>
2021-08-12 13:11:04 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '85f32dff64c80a9da5503f09972cf353'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4242d8e9-3def-4e36-82f1-34df8aed19f6'
+};
